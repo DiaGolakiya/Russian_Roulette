793a28fe80c4c9374e16c4b70ddd7e1be9609b9c
DiaNeal <diagolakiya@gmail.com>
2018-08-16 10:09:41 +1200

    Start

--- Russian_Roulette/Form1.cs
+++ Russian_Roulette/Form1.cs
@@ -16,5 +16,10 @@ namespace Russian_Roulette
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
